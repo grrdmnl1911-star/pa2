47c522b9905b4356b5836f67f4e63b2465fdf32e
grrdmnl1911-star <grrdmnl.19.11@gmail.com>
2026-05-17 01:03:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import pickle
+
 
 st.title("Predicción de especie de flor Iris")
 
@@ -8,7 +10,8 @@ st.write("Nombre: Gerardo Arias Alzamora")
 st.write("Código ISIL: 45443379")
 st.write("[Ver cuaderno en Google Colab](https://colab.research.google.com/drive/1pTMeKeg5Lo6yUd3Us0BLZKuKlQ3ncBdw?usp=sharing)")
 
-modelo = joblib.load("modelos/modelo_random_forest.pkl")
+with open("modelos/modelo_random_forest.pkl", "rb") as archivo:
+    modelo = pickle.load(archivo)
 
 st.subheader("Ingrese los datos de la flor")
 
